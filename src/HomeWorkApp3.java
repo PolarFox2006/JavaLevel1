d7f6561cd05a041530f736ad9d403dd1458a513a
Smallet84 <Sabbath2006@yandex.ru>
2022-06-14 22:21:52 +0300

    Initial commit

---
+++ src/HomeWorkApp3.java
@@ -0,0 +1,77 @@
+public class HomeWorkApp3 {
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("Number 1: ");
+        number1(arr1);
+        System.out.println("\nNumber 2: ");
+        number2(100);
+        System.out.println("\nNumber 3: ");
+        number3(arr3);
+        System.out.println("\nNumber 4: ");
+        number4();
+        System.out.println("\nNumber 5: ");
+        number5(5, 1);
+
+    }
+
+
+    public static void number1(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+    public static void number2(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+    public static void number3(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+    public static void number4() {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    public static void number5(int len, int initialValue){
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+            System.out.print("[" + i + "]" + arr[i] + " ");
+        }
+    }
+
+}
